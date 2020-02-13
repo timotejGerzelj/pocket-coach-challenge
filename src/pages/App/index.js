fe890ad86bfc19e72aff0b10611bb23a8e73963d
timo777 <timotej.gerzelj@gmail.com>
2020-02-13 13:58:46 +0100

    Generating data with a set interval

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,14 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 
-var offers = {};
-var dd = new Date();
-for (var i=0; i< 10; i++) {
-    offers[i] = {"id":i, "date": new Date(dd.setMinutes(dd.getMinutes() + i*10)), "amount": Math.random()*10,
-    "bid": 9.5 + Math.random()};
+function generateOffer () {
+     var offers = {};
+     var dd = new Date();
+     for (var i=0; i< 10; i++) {
+          offers[i] = {"id":i, "date": new Date(dd.setMinutes(dd.getMinutes() + i*10)), "amount": Math.random()*10,
+          "bid": 9.5 + Math.random()};
+     }
+     return offers
 }
 
+const App = () => {
+     const [offers, updateOffers ] = useState([generateOffer()])
+     useEffect(() => {
+          const interval = setInterval(() => updateOffers( offers => ([...offers, generateOffer()])) , 5000)
+          return () => {
+               clearInterval(interval)
+          };
+     }, [])
 
-const App = () => <Fragment>
+     
+     return <Fragment>
           <table>  
           <thead>
                <tr>
@@ -18,17 +30,13 @@ const App = () => <Fragment>
                </tr>
           </thead> 
           <tbody>
-          {
-               Object.keys(offers).map( offer => {
-                    return( <tr key={offers[offer].bid} >
-                              <td>{offers[offer].bid}</td>
-                              <td>{offers[offer].date.toString()}</td>
-                              <td>{offers[offer].amount}</td>
-                         </tr>)
-               }  )
-          }
+               {offers.map(offer => Object.keys(offer).map(o => <tr key={offer[o].bid}>
+                    <td>{offer[o].bid}</td>
+                    <td>{offer[o].date.toString()}</td>
+                    <td>{offer[o].amount}</td>
+               </tr>))}
           </tbody>
           </table>
-     </Fragment>
+     </Fragment>} 
 
 export default App
